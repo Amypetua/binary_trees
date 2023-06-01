fa58257488c64ae17f49953948fb489310d05fb6
Amypetua <anachusichiamaka32@gmail.com>
2023-06-01 14:16:30 -0700

    Insert/Height

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -37,4 +37,3 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		new_node->left->parent = new_node;
 	return (new_node);
 }
->>>>>>> 65aa1dfc3aadcbff1c21521258f4984caaaf95c0
