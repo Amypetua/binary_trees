579ecf790dc2c8fda59e429167f0b970e73c1995
Amypetua <anachusichiamaka32@gmail.com>
2023-06-02 14:03:37 -0700

    Binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,29 +1,28 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height_b - measure height of a binry tree for balance tree
+ * binary_tree_height_balance - measure height of a binry tree for balance tree
  * @tree: tree to go through
  *
  * Return: the balanced factor
  */
-size_t binary_tree_height_b(const binary_tree_t *tree)
+size_t binary_tree_height_balance(const binary_tree_t *tree)
 {
 	size_t left = 0, right = 0;
 
 	if (tree == NULL)
-	{
+
 
 		return (0);
-	}
 
 	else
 	{
 		if (tree)
 		{
-			left = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
-			right = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
+			left = tree->left ? 1 + binary_tree_height_balance(tree->left) : 1;
+			right = tree->right ? 1 + binary_tree_height_balance(tree->right) : 1;
 		}
-		return ((left > right) ? 1 : right);
+		return ((left > right) ? left : right);
 	}
 }
 
